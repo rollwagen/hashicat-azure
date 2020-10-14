e56c59ef7019c4a62480850cf11a0bc289e8fca4
rollwagen <rollwagen@gmail.com>
2020-10-14 23:46:36 +0200

    Fixed resource group name.

--- vnet.tf
+++ vnet.tf
@@ -6,7 +6,7 @@ module "network" {
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
-  resource_group_name = "rollwagen-hashicat-vnet"
+  resource_group_name = "${var.prefix}-workshop"
 
 }
 
